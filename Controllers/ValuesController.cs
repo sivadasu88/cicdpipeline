4f991e21dbab475ac998ef091627b33d51f02461
sivadasu88 <35655707+sivadasu88@users.noreply.github.com>
2019-03-07 10:43:31 +0530

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace cicdpipeline1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value2", "value1" };
         }
 
         // GET api/values/5
